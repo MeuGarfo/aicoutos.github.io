8cf093d1aa5f9492d43cfde3b442cd99d4c95c9c
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-07-24 21:01:53 -0300

    M  index.php

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
 <head>
-<meta charset="utf-8">
-<title>Anderson Ismael</title>
-<?php
-require 'inc/header.php';
-?>
+    <meta charset="utf-8">
+    <title>Anderson Ismael</title>
+    <?php
+    require 'inc/header.php';
+    ?>
 </head>
 <body>
-<h1>Anderson Ismael</h1>
-<h2>Projetos</h2>
-<ul>
-<li><a href="projeto/alexa.php">Alexa Rank</a></li>
-</ul>
-<?php
-require 'inc/footer.php';
-?>
+    <h1>Anderson Ismael</h1>
+    <h2>Projetos</h2>
+    <ul>
+        <li><a href="projeto/alexa.php">Alexa Rank</a></li>
+    </ul>
+    <h2>Hacker Gaucho</h2>
+    <ul>
+        <li><a href="http://geocities.ws/hackergaucho/">Versão antiga</a></li>
+    </ul>
+    <?php
+    require 'inc/footer.php';
+    ?>
 </body>
 </html>
